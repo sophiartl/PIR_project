d427ab07d0aee97b751ea8c22b619133f997413d
sophia <sartioli@daisen.inf.ethz.ch>
2023-01-02 09:44:29 +0100

    fix 4KB

--- commands_simple.sh
+++ commands_simple.sh
@@ -2,9 +2,8 @@
 
 cd  simplepir-main/pir
 
-LOG_N=14 D=2048 go test -bench PirSingle -timeout 0 -run=^$ > bench-14_256B  &&
-LOG_N=14 D=8192 go test -bench PirSingle -timeout 0 -run=^$ > bench-14_1KB &&
-LOG_N=14 D=329768 go test -bench PirSingle -timeout 0 -run=^$ > bench-14_4KB  &&
+
+LOG_N=14 D=32768 go test -bench PirSingle -timeout 0 -run=^$ > bench-14_4KB  &&
 LOG_N=14 D=65536 go test -bench PirSingle -timeout 0 -run=^$ > bench-14_8KB &&
 LOG_N=14 D=131072 go test -bench PirSingle -timeout 0 -run=^$ > bench-14_16KB &&
 LOG_N=14 D=262144 go test -bench PirSingle -timeout 0 -run=^$ > bench-14_32KB  &&
@@ -15,7 +14,7 @@ LOG_N=14 D=2097152 go test -bench PirSingle -timeout 0 -run=^$ > bench-14_256KB 
 
 LOG_N=15 D=2048 go test -bench PirSingle -timeout 0 -run=^$ > bench-15_256B  &&
 LOG_N=15 D=8192 go test -bench PirSingle -timeout 0 -run=^$ > bench-15_1KB &&
-LOG_N=15 D=329768 go test -bench PirSingle -timeout 0 -run=^$ > bench-15_4KB  &&
+LOG_N=15 D=32768 go test -bench PirSingle -timeout 0 -run=^$ > bench-15_4KB  &&
 LOG_N=15 D=65536 go test -bench PirSingle -timeout 0 -run=^$ > bench-15_8KB &&
 LOG_N=15 D=131072 go test -bench PirSingle -timeout 0 -run=^$ > bench-15_16KB &&
 LOG_N=15 D=262144 go test -bench PirSingle -timeout 0 -run=^$ > bench-15_32KB  &&
@@ -26,7 +25,7 @@ LOG_N=15 D=2097152 go test -bench PirSingle -timeout 0 -run=^$ > bench-15_256KB 
 
 LOG_N=16 D=2048 go test -bench PirSingle -timeout 0 -run=^$ > bench-16_256B  &&
 LOG_N=16 D=8192 go test -bench PirSingle -timeout 0 -run=^$ > bench-16_1KB &&
-LOG_N=16 D=329768 go test -bench PirSingle -timeout 0 -run=^$ > bench-16_4KB  &&
+LOG_N=16 D=32768 go test -bench PirSingle -timeout 0 -run=^$ > bench-16_4KB  &&
 LOG_N=16 D=65536 go test -bench PirSingle -timeout 0 -run=^$ > bench-16_8KB &&
 LOG_N=16 D=131072 go test -bench PirSingle -timeout 0 -run=^$ > bench-16_16KB &&
 LOG_N=16 D=262144 go test -bench PirSingle -timeout 0 -run=^$ > bench-16_32KB  &&
@@ -36,7 +35,7 @@ LOG_N=16 D=2097152 go test -bench PirSingle -timeout 0 -run=^$ > bench-16_256KB 
 
 LOG_N=17 D=2048 go test -bench PirSingle -timeout 0 -run=^$ > bench-17_256B  &&
 LOG_N=17 D=8192 go test -bench PirSingle -timeout 0 -run=^$ > bench-17_1KB &&
-LOG_N=17 D=329768 go test -bench PirSingle -timeout 0 -run=^$ > bench-17_4KB  &&
+LOG_N=17 D=32768 go test -bench PirSingle -timeout 0 -run=^$ > bench-17_4KB  &&
 LOG_N=17 D=65536 go test -bench PirSingle -timeout 0 -run=^$ > bench-17_8KB &&
 LOG_N=17 D=131072 go test -bench PirSingle -timeout 0 -run=^$ > bench-17_16KB &&
 LOG_N=17 D=262144 go test -bench PirSingle -timeout 0 -run=^$ > bench-17_32KB  &&
@@ -46,7 +45,7 @@ LOG_N=17 D=2097152 go test -bench PirSingle -timeout 0 -run=^$ > bench-17_256KB 
 
 LOG_N=18 D=2048 go test -bench PirSingle -timeout 0 -run=^$ > bench-18_256B  &&
 LOG_N=18 D=8192 go test -bench PirSingle -timeout 0 -run=^$ > bench-18_1KB &&
-LOG_N=18 D=329768 go test -bench PirSingle -timeout 0 -run=^$ > bench-18_4KB  &&
+LOG_N=18 D=32768 go test -bench PirSingle -timeout 0 -run=^$ > bench-18_4KB  &&
 LOG_N=18 D=65536 go test -bench PirSingle -timeout 0 -run=^$ > bench-18_8KB &&
 LOG_N=18 D=131072 go test -bench PirSingle -timeout 0 -run=^$ > bench-18_16KB &&
 LOG_N=18 D=262144 go test -bench PirSingle -timeout 0 -run=^$ > bench-18_32KB  &&
@@ -56,7 +55,7 @@ LOG_N=18 D=2097152 go test -bench PirSingle -timeout 0 -run=^$ > bench-18_256KB 
 
 LOG_N=19 D=2048 go test -bench PirSingle -timeout 0 -run=^$ > bench-19_256B  &&
 LOG_N=19 D=8192 go test -bench PirSingle -timeout 0 -run=^$ > bench-19_1KB &&
-LOG_N=19 D=329768 go test -bench PirSingle -timeout 0 -run=^$ > bench-19_4KB  &&
+LOG_N=19 D=32768 go test -bench PirSingle -timeout 0 -run=^$ > bench-19_4KB  &&
 LOG_N=19 D=65536 go test -bench PirSingle -timeout 0 -run=^$ > bench-19_8KB &&
 LOG_N=19 D=131072 go test -bench PirSingle -timeout 0 -run=^$ > bench-19_16KB &&
 LOG_N=19 D=262144 go test -bench PirSingle -timeout 0 -run=^$ > bench-19_32KB  &&
@@ -65,7 +64,7 @@ LOG_N=19 D=1048576 go test -bench PirSingle -timeout 0 -run=^$ > bench-19_128KB 
 
 LOG_N=20 D=2048 go test -bench PirSingle -timeout 0 -run=^$ > bench-20_256B  &&
 LOG_N=20 D=8192 go test -bench PirSingle -timeout 0 -run=^$ > bench-20_1KB &&
-LOG_N=20 D=329768 go test -bench PirSingle -timeout 0 -run=^$ > bench-20_4KB  &&
+LOG_N=20 D=32768 go test -bench PirSingle -timeout 0 -run=^$ > bench-20_4KB  &&
 LOG_N=20 D=65536 go test -bench PirSingle -timeout 0 -run=^$ > bench-20_8KB &&
 LOG_N=20 D=131072 go test -bench PirSingle -timeout 0 -run=^$ > bench-20_16KB &&
 LOG_N=20 D=262144 go test -bench PirSingle -timeout 0 -run=^$ > bench-20_32KB  &&
@@ -74,7 +73,7 @@ LOG_N=20 D=1048576 go test -bench PirSingle -timeout 0 -run=^$ > bench-20_128KB 
 
 LOG_N=21 D=2048 go test -bench PirSingle -timeout 0 -run=^$ > bench-21_256B  &&
 LOG_N=21 D=8192 go test -bench PirSingle -timeout 0 -run=^$ > bench-21_1KB &&
-LOG_N=21 D=329768 go test -bench PirSingle -timeout 0 -run=^$ > bench-21_4KB  &&
+LOG_N=21 D=32768 go test -bench PirSingle -timeout 0 -run=^$ > bench-21_4KB  &&
 LOG_N=21 D=65536 go test -bench PirSingle -timeout 0 -run=^$ > bench-21_8KB &&
 LOG_N=21 D=131072 go test -bench PirSingle -timeout 0 -run=^$ > bench-21_16KB &&
 LOG_N=21 D=262144 go test -bench PirSingle -timeout 0 -run=^$ > bench-21_32KB  &&
@@ -83,7 +82,7 @@ LOG_N=21 D=1048576 go test -bench PirSingle -timeout 0 -run=^$ > bench-21_128KB 
 
 LOG_N=22 D=2048 go test -bench PirSingle -timeout 0 -run=^$ > bench-22_256B  &&
 LOG_N=22 D=8192 go test -bench PirSingle -timeout 0 -run=^$ > bench-22_1KB &&
-LOG_N=22 D=329768 go test -bench PirSingle -timeout 0 -run=^$ > bench-22_4KB  &&
+LOG_N=22 D=32768 go test -bench PirSingle -timeout 0 -run=^$ > bench-22_4KB  &&
 LOG_N=22 D=65536 go test -bench PirSingle -timeout 0 -run=^$ > bench-22_8KB &&
 LOG_N=22 D=131072 go test -bench PirSingle -timeout 0 -run=^$ > bench-22_16KB &&
 LOG_N=22 D=262144 go test -bench PirSingle -timeout 0 -run=^$ > bench-22_32KB  &&
